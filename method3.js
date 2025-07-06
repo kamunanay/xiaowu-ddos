638d03a67702a59030e621d9d500ffa6bfa93065
M.Galih Pratama <kadal.rigel@gmail.com>
2025-07-06 08:56:37 +0700

    Update method3.js

--- method3.js
+++ method3.js
@@ -1,38 +1,22 @@
-(function(_0x小吴1, _0x小吴2) {
-    const _0x小吴3 = function(_0x小吴4) {
-        while (--_0x小吴4) {
-            _0x小吴1.push(_0x小吴1.shift());
-        }
-    };
-    _0x小吴3(++_0x小吴2);
-}(_0x小吴数组, 0x1f4));
+const 小吴随机 = require('randomstring');
+const 小吴参数 = process.argv.slice(2);
 
-const _0x小吴解码 = function(_0x小吴5, _0x小吴6) {
-    _0x小吴5 = _0x小吴5 - 0x0;
-    return _0x小吴数组[_0x小吴5];
-};
-
-const 小吴随机=require(_0x小吴解码('0x0'));
-const 小吴参数=process[_0x小吴解码('0x1')]['slice'](0x2);
-
-if(小吴参数['length']<0x3){
-    console[_0x小吴解码('0x2')](_0x小吴解码('0x3'));
-    process[_0x小吴解码('0x4')](-0x1);
+if (小吴参数.length < 3) {
+    console.log("Usage: node method3.js <url> <time> <rate>");
+    process.exit(-1);
 }
 
-const 小吴网址=小吴参数[0x0];
-const 小吴时间=parseInt(小吴参数[0x1]);
-const 小吴速率=parseInt(小吴参数[0x2]);
+const 小吴网址 = 小吴参数[0], 小吴时间 = parseInt(小吴参数[1]), 小吴速率 = parseInt(小吴参数[2]);
 
-console[_0x小吴解码('0x2')](`🔥 TCP SYN Flood: ${小吴网址} | Duration: ${小吴时间}s | Rate: ${小吴速率} packets/s`);
+console.log(`🔥 TCP SYN Flood ke ${小吴网址} selama ${小吴时间}s @ ${小吴速率} packet/s`);
 
-const 小吴间隔=setInterval(()=>{
-    for(let 小吴i=0x0;小吴i<小吴速率;小吴i++){
-        console[_0x小吴解码('0x2')](`📡 SYN Packet sent to ${小吴网址} with random seq=${小吴随机['generate'](0x8)}`);
+const 小吴循环 = setInterval(() => {
+    for (let 小吴i = 0; 小吴i < 小吴速率; 小吴i++) {
+        console.log(`📡 SYN Packet ke ${小吴网址} seq=${小吴随机.generate(8)}`);
     }
-},0x3e8);
+}, 1000);
 
-setTimeout(()=>{
-    clearInterval(小吴间隔);
-    console[_0x小吴解码('0x2')]("✅ TCP SYN Flood selesai!");
-},小吴时间*0x3e8);
+setTimeout(() => {
+    clearInterval(小吴循环);
+    console.log("✅ TCP SYN Flood 完成!");
+}, 小吴时间 * 1000);
